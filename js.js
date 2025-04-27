b2473ce0fa7a3fe002f153988da01aed50252d5d
DolimovS <samandardolimov71@gmail.com>
2025-04-27 13:47:03 +0500

    korzinkaga locelStrogedan maxsulotlar qoshilishi yakunlandi

--- js.js
+++ js.js
@@ -13,7 +13,7 @@ const getData = async () => {
         // console.log(element);
         
         katogoriya.innerHTML += `
-         <button>
+            <button>
                 <img  src="${element.image}" alt="">
                 <p>${element.name}</p>
             </button>
@@ -48,14 +48,41 @@ const burger=async()=>{
 
 burger()
 
-//burger button onclick bolganda
-const productBuy=document.querySelector(".product_buy")
-const data=[]
+//burger button onclick bolganda localStroge ga joylash
 
-const buttonOnclick=((id)=>{
-    korzinka_arr.forEach((item)=>{
-        if(item.id===id){
-            data.push(item)
+const data=JSON.parse(localStorage.getItem("productData"))||[]
+
+const buttonOnclick=(id)=>{
+    let allproduct=korzinka_arr.find(item=>item.id==id)
+    let dataProduct=data.find(item=>item.id==id)
+    
+    if(dataProduct){
+        dataProduct.count++
+    }
+    else{
+        data.push({...allproduct,count:1})
+    }
+
+    // localStorage.removeItem("data")
+
+    localStorage.setItem("productData",JSON.stringify(data))
+// console.log(data);
+}
+
+//lokalStroge dan karzinkaga olish
+
+const productBuy=document.querySelector(".product_buy")
+
+let jami=0
+let jami_summa=0
+
+setInterval(()=>{
+    let localProduct=JSON.parse(localStorage.getItem("productData"))
+    productBuy.innerHTML=""
+    jami=0
+    jami_summa=0
+    if(localProduct){
+        localProduct.forEach((item)=>{
             productBuy.innerHTML+=`
             <div class="box">
                 <div class="left">
@@ -66,13 +93,64 @@ const buttonOnclick=((id)=>{
                             <p class="sum">${item.summa}₽</p>
                         </div>
                 </div>
-                <div class="box_button"><button>-</button><span>00</span><button>+</button></div>
+                <div class="box_button"><button>-</button><span>${item.count}</span><button>+</button></div>
             </div>
-            `
+            `;
+            jami+=item.count
+            jami_summa+=item.count*item.summa
         }
-    })
-    console.log(data);
-})
+        )
+        
+    }
+
+    //karzinkadagi mahsulotlar soni
+    let praduct_soni=document.querySelector(".praduct_soni")
+    praduct_soni.textContent=jami
+
+    //karzingadagi mahsulotlar  narxi jami
+    let jami_narxi=document.querySelector(".jami_narxi")
+    jami_narxi.textContent=`${jami_summa}₽`
+    // console.log(jami_summa);
+},100)
+
+
+
+
+// let localProduct=JSON.parse(localStorage.getItem("productData"))
+
+// console.log(localProduct);
+
+// localProduct.forEach((item)=>{
+
+//     productBuy.innerHTML+=`
+//             <div class="box">
+//                 <div class="left">
+//                     <img src="${item.image}" alt="">
+//                         <div class="box_center">
+//                             <p class="nomi">${item.name}</p>
+//                             <p class="massa">${item.massa}г</p>
+//                             <p class="sum">${item.summa}₽</p>
+//                         </div>
+//                 </div>
+//                 <div class="box_button"><button>-</button><span>${item.count}</span><button>+</button></div>
+//     </div>
+//             `
+// })
+
+            // productBuy.innerHTML+=`
+            // <div class="box">
+            //     <div class="left">
+            //         <img src="${item.image}" alt="">
+            //             <div class="box_center">
+            //                 <p class="nomi">${item.name}</p>
+            //                 <p class="massa">${item.massa}г</p>
+            //                 <p class="sum">${item.summa}₽</p>
+            //             </div>
+            //     </div>
+            //     <div class="box_button"><button>-</button><span>00</span><button>+</button></div>
+            // </div>
+            // `
+
 
 
 
